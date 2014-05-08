6c515012f78ec8224414aa2c0b938786db867716
Andres Vargas <andres.vargas@interalia.net>
2014-05-08 16:49:22 -0500

    reorganize code

--- daemon/gateway.py
+++ daemon/gateway.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 
-import logging
 import tornado.options
 import tornado.web
 import tornado.websocket
-import os.path
 import obelisk
-import json
 import threading
 import code
-from collections import defaultdict
 
 import config
 
@@ -27,6 +23,7 @@ parse_command_line()
 
 import rest_handlers
 import obelisk_handler
+import querysocket_handler
 import jsonchan
 import broadcast
 import ticker
@@ -49,7 +46,7 @@ class GatewayApplication(tornado.web.Application):
         self.p2p.join_network(config.get('seeds', []))
         self.json_chan_handler = jsonchan.JsonChanHandler(self.p2p)
         self.ticker_handler = ticker.TickerHandler()
-
+        #websocket uri space
         handlers = [
             # /block/<block hash>
             (r"/block/([^/]*)(?:/)?", rest_handlers.BlockHeaderHandler),
@@ -71,82 +68,17 @@ class GatewayApplication(tornado.web.Application):
             (r"/height(?:/)?", rest_handlers.HeightHandler),
 
             # /
-            (r"/", QuerySocketHandler)
+            (r"/", querysocket_handler.QuerySocketHandler)
         ]
 
-        tornado.web.Application.__init__(self, handlers, **settings)
+        # helloobelisk uri space
+        uri_space= r"/rest/v1/"
+        other_handlers = [
+            (uri_space + 'addresses/([^/]*)(?:/)?', rest_handlers.AddressHistoryHandler),
+        ]
+        all_handlers = other_handlers + handlers
+        tornado.web.Application.__init__(self, all_handlers, **settings)
 
-class QuerySocketHandler(tornado.websocket.WebSocketHandler):
-
-    # Set of WebsocketHandler
-    listeners = set()
-    # Protects listeners
-    listen_lock = threading.Lock()
-
-    def initialize(self):
-        self._obelisk_handler = self.application.obelisk_handler
-        self._brc_handler = self.application.brc_handler
-        self._json_chan_handler = self.application.json_chan_handler
-        self._ticker_handler = self.application.ticker_handler
-        self._subscriptions = defaultdict(dict)
-        self._connected = False
-
-    def open(self):
-        logging.info("OPEN")
-        with QuerySocketHandler.listen_lock:
-            self.listeners.add(self)
-        self._connected = True
-
-    def on_close(self):
-        logging.info("CLOSE")
-        disconnect_msg = {'command': 'disconnect_client', 'id': 0, 'params': []}
-        self._connected = False
-        self._obelisk_handler.handle_request(self, disconnect_msg)
-        self._json_chan_handler.handle_request(self, disconnect_msg)
-        with QuerySocketHandler.listen_lock:
-            self.listeners.remove(self)
-
-    def _check_request(self, request):
-        return request.has_key("command") and request.has_key("id") and \
-            request.has_key("params") and type(request["params"]) == list
-
-    def on_message(self, message):
-        try:
-            request = json.loads(message)
-        except:
-            logging.error("Error decoding message: %s", message, exc_info=True)
-
-        # Check request is correctly formed.
-        if not self._check_request(request):
-            logging.error("Malformed request: %s", request, exc_info=True)
-            return
-        # Try different handlers until one accepts request and
-        # processes it.
-        if self._json_chan_handler.handle_request(self, request):
-            return
-        if self._obelisk_handler.handle_request(self, request):
-            return
-        if self._brc_handler.handle_request(self, request):
-            return
-        if self._ticker_handler.handle_request(self, request):
-            return
-        logging.warning("Unhandled command. Dropping request: %s",
-            request, exc_info=True)
-
-    def _send_response(self, response):
-        try:
-            self.write_message(json.dumps(response))
-        except tornado.websocket.WebSocketClosedError:
-            self._connected = False
-            logging.warning("Dropping response to closed socket: %s",
-               response, exc_info=True)
-
-    def queue_response(self, response):
-        try:
-            # calling write_message or the socket is not thread safe
-            ioloop.add_callback(self._send_response, response)
-        except:
-            logging.error("Error adding callback", exc_info=True)
 
 class DebugConsole(threading.Thread):
 
@@ -170,5 +102,8 @@ def main(service):
 
 if __name__ == "__main__":
     service = config.get("obelisk-url", "tcp://127.0.0.1:9091")
-    main(service)
+    try:
+        main(service)
+    except KeyboardInterrupt:
+        reactor.stop()
 
---
+++ daemon/querysocket_handler.py
@@ -0,0 +1,78 @@
+import tornado
+import logging
+import threading
+from collections import defaultdict
+
+
+class QuerySocketHandler(tornado.websocket.WebSocketHandler):
+
+    # Set of WebsocketHandler
+    listeners = set()
+    # Protects listeners
+    listen_lock = threading.Lock()
+
+    def initialize(self):
+        self._obelisk_handler = self.application.obelisk_handler
+        self._brc_handler = self.application.brc_handler
+        self._json_chan_handler = self.application.json_chan_handler
+        self._ticker_handler = self.application.ticker_handler
+        self._subscriptions = defaultdict(dict)
+        self._connected = False
+
+    def open(self):
+        logging.info("OPEN")
+        with QuerySocketHandler.listen_lock:
+            self.listeners.add(self)
+        self._connected = True
+
+    def on_close(self):
+        logging.info("CLOSE")
+        disconnect_msg = {'command': 'disconnect_client', 'id': 0, 'params': []}
+        self._connected = False
+        self._obelisk_handler.handle_request(self, disconnect_msg)
+        self._json_chan_handler.handle_request(self, disconnect_msg)
+        with QuerySocketHandler.listen_lock:
+            self.listeners.remove(self)
+
+    def _check_request(self, request):
+        return request.has_key("command") and request.has_key("id") and \
+            request.has_key("params") and type(request["params"]) == list
+
+    def on_message(self, message):
+        try:
+            request = json.loads(message)
+        except:
+            logging.error("Error decoding message: %s", message, exc_info=True)
+
+        # Check request is correctly formed.
+        if not self._check_request(request):
+            logging.error("Malformed request: %s", request, exc_info=True)
+            return
+        # Try different handlers until one accepts request and
+        # processes it.
+        if self._json_chan_handler.handle_request(self, request):
+            return
+        if self._obelisk_handler.handle_request(self, request):
+            return
+        if self._brc_handler.handle_request(self, request):
+            return
+        if self._ticker_handler.handle_request(self, request):
+            return
+        logging.warning("Unhandled command. Dropping request: %s",
+            request, exc_info=True)
+
+    def _send_response(self, response):
+        try:
+            self.write_message(json.dumps(response))
+        except tornado.websocket.WebSocketClosedError:
+            self._connected = False
+            logging.warning("Dropping response to closed socket: %s",
+               response, exc_info=True)
+
+    def queue_response(self, response):
+        try:
+            # calling write_message or the socket is not thread safe
+            ioloop.add_callback(self._send_response, response)
+        except:
+            logging.error("Error adding callback", exc_info=True)
+
